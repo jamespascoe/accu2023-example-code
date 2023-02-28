ca1a28aac58835bdaa65f76f72dfa6daf01f112c
James Pascoe <james.pascoe@bluwireless.com>
2023-02-28 07:50:45 +0000

    Added small improvments to Async server

--- async_http_server.cpp
+++ async_http_server.cpp
@@ -9,18 +9,25 @@
 #include <iostream>
 #include <format>
 
-
-
+namespace asio = boost::asio;
+using tcp = boost::asio::ip::tcp;
 
 int main(int argc, char *argv[])
 {
-  if (argc != 3) {
+  if (argc != 4) {
     std::cerr << std::format(
-      "Usage: {} <ip-address> <port>\nE.g.: {} 0.0.0.0 8080\n",
+      "Usage: {} <ip-address> <port> <threads>\n"
+      "E.g.: {} 0.0.0.0 8080 2\n",
       argv[0], argv[0]
     );
     return EXIT_FAILURE;
   }
+
+  auto const address = asio::ip::make_address(argv[1]);
+  auto const port = static_cast<unsigned short>(std::atoi(argv[2]));
+  auto const threads = std::max<int>(1, std::atoi(argv[3]));
+
+  asio::io_context ioc{threads};
 
 
 }
