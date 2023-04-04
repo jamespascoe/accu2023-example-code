c74693de4acef9cc040b56134c61ee378fa0979a
James Pascoe <james.pascoe@bluwireless.com>
2023-04-04 14:43:02 +0100

    Fixed awaitable

--- beast_example/await_http_server.cpp
+++ beast_example/await_http_server.cpp
@@ -15,16 +15,16 @@
 #include <vector>
 #include <format>
 
-namespace beast = boost::beast;         // from <boost/beast.hpp>
-namespace http = beast::http;           // from <boost/beast/http.hpp>
-namespace net = boost::asio;            // from <boost/asio.hpp>
-using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace asio = boost::asio;
+using tcp = boost::asio::ip::tcp;
 
 using tcp_stream = typename beast::tcp_stream::rebind_executor<
-        net::use_awaitable_t<>::executor_with_default<net::any_io_executor>>::other;
+        asio::use_awaitable_t<>::executor_with_default<asio::any_io_executor>>::other;
 
 // Handles an HTTP server connection
-net::awaitable<void> do_session(tcp_stream stream)
+asio::awaitable<void> do_session(tcp_stream stream)
 {
     beast::error_code ec;
 
@@ -46,7 +46,7 @@ net::awaitable<void> do_session(tcp_stream stream)
         auto handle_request = [&req]() -> http::message_generator {
           http::response<http::string_body> res{http::status::ok, req.version()};
           res.set(http::field::server, "Beast");
-          res.body() = "Hello, ACCU 2023!";
+          res.body() = "Hello ACCU 2023 from the Awaitable Server!";
           res.prepare_payload();
           res.keep_alive(req.keep_alive());
           return res;
@@ -57,7 +57,7 @@ net::awaitable<void> do_session(tcp_stream stream)
         bool keep_alive = msg.keep_alive();
 
         // Send the response
-        co_await beast::async_write(stream, std::move(msg), net::use_awaitable);
+        co_await beast::async_write(stream, std::move(msg), asio::use_awaitable);
 
         if(!keep_alive)
         {
@@ -81,20 +81,20 @@ net::awaitable<void> do_session(tcp_stream stream)
 //------------------------------------------------------------------------------
 
 // Accepts incoming connections and launches the sessions
-net::awaitable<void> do_listen(tcp::endpoint endpoint)
+asio::awaitable<void> do_listen(tcp::endpoint endpoint)
 {
     // Open the acceptor
-    auto acceptor = net::use_awaitable.as_default_on(tcp::acceptor(co_await net::this_coro::executor));
+    auto acceptor = asio::use_awaitable.as_default_on(tcp::acceptor(co_await asio::this_coro::executor));
     acceptor.open(endpoint.protocol());
 
     // Allow address reuse
-    acceptor.set_option(net::socket_base::reuse_address(true));
+    acceptor.set_option(asio::socket_base::reuse_address(true));
 
     // Bind to the server address
     acceptor.bind(endpoint);
 
     // Start listening for connections
-    acceptor.listen(net::socket_base::max_listen_connections);
+    acceptor.listen(asio::socket_base::max_listen_connections);
 
     for(;;)
         boost::asio::co_spawn(
@@ -127,12 +127,12 @@ int main(int argc, char* argv[])
       return EXIT_FAILURE;
     }
 
-    auto const address = net::ip::make_address(argv[1]);
+    auto const address = asio::ip::make_address(argv[1]);
     auto const port = static_cast<unsigned short>(std::atoi(argv[2]));
     auto const num_threads = std::max<int>(1, std::atoi(argv[3]));
 
     // The io_context is required for all I/O
-    net::io_context ioc{num_threads};
+    asio::io_context ioc{num_threads};
 
     // Spawn a listening port
     boost::asio::co_spawn(ioc,
